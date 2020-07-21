4019d625cb52482da2950fbac0b512d0565f322f
sriman577 <17pa1a0577@vishnu.edu.in>
2020-07-21 20:42:49 +0530

    second commit

---
+++ Lambdas-and-Streams/src/main/java/com/epam/Lambdas_and_Streams/Average.java
@@ -0,0 +1,8 @@
+package com.epam.Lambdas_and_Streams;
+import java.util.*;
+public class Average {
+	public double avg(List<Integer> list)
+	{
+		return list.stream().mapToInt(number -> number.intValue()).average().getAsDouble();
+	}
+}
